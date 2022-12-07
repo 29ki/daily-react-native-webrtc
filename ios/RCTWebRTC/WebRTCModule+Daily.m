c9ec9a8e6c1ff160c7f58d173853b3e1fa09a952
Jakob Öhman <jakob@29k.org>
2022-12-07 17:19:20 +0100

    Properly mixWithOthers on iOS 16

--- ios/RCTWebRTC/WebRTCModule+Daily.m
+++ ios/RCTWebRTC/WebRTCModule+Daily.m
@@ -152,8 +152,8 @@ RCT_EXPORT_METHOD(setDailyAudioMode:(NSString *)audioMode) {
   // Ducking other apps' audio implicitly enables allowing mixing audio with
   // other apps, which allows this app to stay alive in the backgrounnd during
   // a call (assuming it has the voip background mode set).
-  AVAudioSessionCategoryOptions categoryOptions = (AVAudioSessionCategoryOptionAllowBluetooth |
-                                                   AVAudioSessionCategoryOptionDuckOthers);
+  AVAudioSessionCategoryOptions categoryOptions = (AVAudioSessionCategoryOptionMixWithOthers |
+                                                   AVAudioSessionCategoryOptionAllowBluetooth);
   if ([audioMode isEqualToString:AUDIO_MODE_VIDEO_CALL]) {
     categoryOptions |= AVAudioSessionCategoryOptionDefaultToSpeaker;
   }
